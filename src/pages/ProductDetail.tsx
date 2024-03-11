eba407769dfb75a943f6a8e66090b665ff103ab1
Shelly_Wu <shellywu37@gmail.com>
2024-03-11 17:11:57 -0400

    applied style to productdetail

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -1,17 +1,79 @@
+import { Box, Grid, Typography, Button } from "@mui/material";
+
 const ProductDetail = () => {
   return (
-    <div>
-      <img src="https://www.cookmuseum.org/wp-content/uploads/2020/09/air-image-cropped.jpg"></img>
-      <h2>Canada Air</h2>
-      <h3>Decription</h3>
-      <p>
-        The Canada Air, a breath of fresh, mmerse yourself in the essence of the
-        Great White North with our premium-quality air. Buy it now! Why wait?
-      </p>
-      <p>99999</p>
-      <button>Edit</button>
-      <button>Delete</button>
-    </div>
+    <Box
+      sx={{
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        marginTop: "50px",
+      }}
+    >
+      <Grid container spacing={10} maxWidth="1200px">
+        <Grid item xs={12} md={6}>
+          <img
+            src="https://www.cookmuseum.org/wp-content/uploads/2020/09/air-image-cropped.jpg"
+            alt="Product Image"
+            style={{ width: "100%", height: "auto" }}
+          />
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          md={6}
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            "flex-direction": "column",
+          }}
+        >
+          <Typography variant="h4" component="h2" gutterBottom align="center">
+            Canada Snowy Scent
+          </Typography>
+          <Typography
+            variant="body1"
+            component="p"
+            align="center"
+            sx={{ color: "#a7a7a7" }}
+          >
+            A breath of fresh, 100% Nature.
+          </Typography>
+          <Typography variant="h5" component="h3" gutterBottom align="center">
+            CA$ 99999
+          </Typography>
+          <Typography variant="body1" component="p" align="left">
+            The Canada Air, a breath of fresh, mmerse yourself in the essence of
+            the Great White North with our premium-quality air. Buy it now! Why
+            wait?
+          </Typography>
+          <Box sx={{ display: "flex", justifyContent: "center", gap: 2 }}>
+            <Button
+              variant="contained"
+              color="primary"
+              sx={{
+                backgroundColor: "#027a9c",
+                "&:hover": { backgroundColor: "#00566e" },
+              }}
+            >
+              Edit
+            </Button>
+            <Button
+              variant="contained"
+              color="secondary"
+              sx={{
+                backgroundColor: "#027a9c",
+                "&:hover": { backgroundColor: "#00566e" },
+              }}
+            >
+              Delete
+            </Button>
+          </Box>
+        </Grid>
+      </Grid>
+    </Box>
   );
 };
 
