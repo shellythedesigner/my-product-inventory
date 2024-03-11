84e8453a83425d75c9fea8f6d9b9b12be49e1394
Shelly_Wu <shellywu37@gmail.com>
2024-03-11 15:46:27 -0400

    Update the style on navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,24 +1,69 @@
 import { Link } from "react-router-dom";
+import { AppBar, Toolbar, Typography, Button } from "@mui/material";
 
 const Navbar = () => {
   return (
-    <>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/AddProduct">AddProduct</Link>
-        </li>
-        <li>
-          <Link to="/ProductDetail">ProductDetail</Link>
-        </li>
-        <li>
-          <Link to="/Favourites">Favourites</Link>
-        </li>
-      </ul>
-    </>
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Air
+        </Typography>
+        <nav>
+          <Button
+            component={Link}
+            to="/"
+            sx={{ textDecoration: "none", color: "inherit" }}
+          >
+            Home
+          </Button>
+          <Button
+            component={Link}
+            to="/AddProduct"
+            sx={{ textDecoration: "none", color: "inherit" }}
+          >
+            Add Product
+          </Button>
+          <Button
+            component={Link}
+            to="/ProductDetail"
+            sx={{ textDecoration: "none", color: "inherit" }}
+          >
+            Product Detail
+          </Button>
+          <Button
+            component={Link}
+            to="/Favourites"
+            sx={{ textDecoration: "none", color: "inherit" }}
+          >
+            Favourites
+          </Button>
+        </nav>
+      </Toolbar>
+    </AppBar>
   );
 };
 
 export default Navbar;
+
+// const Navbar = () => {
+//   return (
+//     <>
+//       <ul>
+//         <li>
+//           <Link to="/">Home</Link>
+//         </li>
+//         <li>
+//           <Link to="/AddProduct">AddProduct</Link>
+//         </li>
+//         <li>
+//           <Link to="/ProductDetail">ProductDetail</Link>
+//         </li>
+//         <li>
+//           <Link to="/Favourites">Favourites</Link>
+//         </li>
+//       </ul>
+//     </>
+//   );
+// };
+
+// export default Navbar;
